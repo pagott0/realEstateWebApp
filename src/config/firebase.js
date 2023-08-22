d9b624bda13fba5a16d1dc88d2d98b349c4d5348
pagott0 <enzovaspagotto@gmail.com>
2023-08-22 19:44:51 -0300

    configured firebase emulators and added auth to localhost

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, connectAuthEmulator } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+connectAuthEmulator(auth, "http://127.0.0.1:9099");
